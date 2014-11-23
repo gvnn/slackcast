219dd6207f0183bad09bd3e98d0722391d790b27
Giovanni Ferron <gvnnrulez@gmail.com>
2014-11-24 09:41:47 +1100

    Add search activity
    Added search activity with toolbar back button

--- app/src/main/java/it/gvnn/slackcast/MainActivity.java
+++ app/src/main/java/it/gvnn/slackcast/MainActivity.java
@@ -1,10 +1,12 @@
 package it.gvnn.slackcast;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -56,5 +58,21 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
             mNavigationDrawerFragment.closeDrawer();
         else
             super.onBackPressed();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_search) {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/it/gvnn/slackcast/SearchActivity.java
@@ -0,0 +1,33 @@
+package it.gvnn.slackcast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+
+public class SearchActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white);
+        mToolbar.setTitle(R.string.title_activity_search);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
