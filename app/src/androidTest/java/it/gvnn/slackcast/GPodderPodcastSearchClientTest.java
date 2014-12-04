ced989a9e899cc9dc30b59af865dd4150648d009
Giovanni Ferron <gvnnrulez@gmail.com>
2014-12-04 17:26:10 +1100

    Add mock network
    Added mock network class and mock search result to test the parsing

--- app/src/androidTest/java/it/gvnn/slackcast/GPodderPodcastSearchClientTest.java
+++ app/src/androidTest/java/it/gvnn/slackcast/GPodderPodcastSearchClientTest.java
@@ -1,39 +1,34 @@
 package it.gvnn.slackcast;
 
+import android.test.InstrumentationTestCase;
 import android.util.Log;
 
-import com.android.volley.Network;
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.NoCache;
-
-import junit.framework.TestCase;
+import com.google.common.io.ByteStreams;
 
 import java.util.concurrent.CountDownLatch;
 
+import it.gvnn.slackcast.mock.MockNetwork;
 import it.gvnn.slackcast.search.GPodderPodcastSearchClient;
 import it.gvnn.slackcast.search.SearchResultListener;
 
-public class GPodderPodcastSearchClientTest extends TestCase {
+public class GPodderPodcastSearchClientTest extends InstrumentationTestCase {
     private static final String TAG = "GPodderPodcastSearchClientTest";
     final CountDownLatch signal = new CountDownLatch(1);
 
     @Override
     protected void setUp() throws Exception {
-
         super.setUp();
     }
 
     public void testSearch() throws Exception {
-//        MockNetwork network = new MockNetwork();
-//        RequestQueue queue = new RequestQueue(, network);
+        MockNetwork network = new MockNetwork();
 
-        // Set up the network to use HttpURLConnection as the HTTP client.
-        Network network = new BasicNetwork(new HurlStack());
-        // Instantiate the RequestQueue with the cache and network.
+        network.setDataToReturn(ByteStreams.toByteArray(getInstrumentation().getTargetContext().getAssets().open("mock/searchResult.json")));
         RequestQueue queue = new RequestQueue(new NoCache(), network);
+
         // Start the queue
         queue.start();
 
@@ -43,15 +38,15 @@ public class GPodderPodcastSearchClientTest extends TestCase {
             @Override
             public void onResult(PodcastSearchResponse response) {
                 Log.d(TAG, response.toString());
-                assertEquals(0, 1);
+                assertEquals(response.size(), 2);
                 signal.countDown();
             }
 
             @Override
             public void onError(VolleyError error) {
                 Log.d(TAG, error.toString());
-                assertEquals(0, 1);
                 signal.countDown();
+                assertNull(error); // never true
             }
 
         });
