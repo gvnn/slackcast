d011ab195b825d88f1f582a91e310cc69ae45d19
Giovanni Ferron <gvnnrulez@gmail.com>
2015-01-01 20:51:29 +1100

    Add more details to the podcast activity
    Added title and description to the details activity.

--- app/src/main/java/it/gvnn/slackcast/PodcastActivity.java
+++ app/src/main/java/it/gvnn/slackcast/PodcastActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+
 import it.gvnn.slackcast.model.Podcast;
+import it.gvnn.slackcast.search.PodcastDataResponse;
+import it.gvnn.slackcast.search.PodcastSearchClient;
+import it.gvnn.slackcast.search.PodcastSearchClientFactory;
+import it.gvnn.slackcast.search.Services;
+import it.gvnn.slackcast.utils.VolleyResultListener;
 
 
 public class PodcastActivity extends ActionBarActivity {
-
+    private static final String TAG = "SearchActivity";
     private Toolbar mToolbar;
     private Podcast mPodcast;
+    private PodcastSearchClient mPodcastSearchClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,35 @@ public class PodcastActivity extends ActionBarActivity {
 
         TextView podcastTitleView = (TextView) findViewById(R.id.podcast_detail_title);
         podcastTitleView.setText(mPodcast.getTitle());
+
+        TextView podcastDescriptionView = (TextView) findViewById(R.id.podcast_detail_description);
+        podcastDescriptionView.setText(mPodcast.getDescription());
+
+        // load podcast info
+        loadPodcastInfo();
+    }
+
+    private void loadPodcastInfo() {
+        Log.i(TAG, String.format("Loading details of %s", mPodcast.getUrl()));
+
+        PodcastSearchClientFactory mPodcastSearchClientFactory = PodcastSearchClientFactory.getInstance(getCacheDir());
+        mPodcastSearchClient = mPodcastSearchClientFactory.getSearchClient(Services.GPODDER);
+        mPodcastSearchClient.getPodcast(mPodcast.getUrl(), new VolleyResultListener<PodcastDataResponse>() {
+            @Override
+            public void onResult(PodcastDataResponse response) {
+                updateUI(response);
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+                Log.e(TAG, error.getMessage());
+            }
+        });
+
+    }
+
+    private void updateUI(PodcastDataResponse response) {
+
     }
 
     @Override
